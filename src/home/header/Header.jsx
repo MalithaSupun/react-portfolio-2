bc3350390fa2d87da07f276ad154412f12b14dbb
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-02-29 09:54:17 +0530

    update

--- src/home/header/Header.jsx
+++ src/home/header/Header.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/logo.png';
 
 
 export default function Header() {
+    const [sidebar,setSidebar] = useState(false)
   return (
     <>
     <header className='header'>
         <div className='container flex'>
-            <div className="logo">
+            <div className='logo'>
                 <img src={logo} alt=''/>
+            </div>
+            <div className='nav'>
+                <ul className={sidebar ? "nav-links-sidebar" : "nav-links" } onClick={() => setSidebar(false)}>
+                    <li>
+                        <a href='/'>Home</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </header>
