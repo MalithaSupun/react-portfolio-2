fccadc1a6a9b59b66b916031566d38e9b084b761
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-03-01 12:27:29 +0530

    Branding Page Created

--- src/components/pageContent/HomePages.jsx
+++ src/components/pageContent/HomePages.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from '../home/homes/Home'
+import Branding from '../home/Branding'
 
 const HomePages = () => {
   return (
     <>
     <Home />
+    <Branding />
     </>
   )
 }
