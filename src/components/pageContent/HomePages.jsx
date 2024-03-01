6b82a5506c345626cc7f24025c203cfaaff8dc3a
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-03-01 13:31:04 +0530

    complete about css

--- src/components/pageContent/HomePages.jsx
+++ src/components/pageContent/HomePages.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from '../home/homes/Home'
 import Branding from '../home/Branding'
+import About from '../home/about/About'
 
 const HomePages = () => {
   return (
     <>
     <Home />
     <Branding />
+    <About />
     </>
   )
 }
