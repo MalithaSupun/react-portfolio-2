eed73e6547ba0476da37b230f984b3f899260cf1
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-03-06 00:43:38 +0530

    just complete service and edit servicedata.js file

--- src/components/home/servicess/Service.jsx
+++ src/components/home/servicess/Service.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import ServiceSData from '../servicess/ServicesData';
+
 
 const Service = () => {
   return (
     <>
         <section className="services topMargin">
-          
+          <div className="container">
+            <div className="heading">
+              <h3>MY SERVICES</h3>
+              <h1>interactive Services offered Me</h1>
+            </div>
+
+            <div className="contain grid topMargin">
+              {ServiceSData.map((val) => {
+                  return ( 
+                  <>
+                  <div className='box'>
+                    <div className='img'>
+                      <img src={val.cover} alt='' />
+                    </div>
+                    <div className="text">
+                      <h2>{val.title}</h2>
+                      <h2>{val.desc}</h2>
+                    </div>
+                  </div>
+                  </>
+                  )
+                })}
+            </div>
+          </div>
         </section>
     </>
   )
--- src/components/home/servicess/ServicesData.js
+++ src/components/home/servicess/ServicesData.js
@@ -1,40 +1,47 @@
+import s1 from '../../../assets/services/services-1.jpg'
+import s2 from '../../../assets/services/services-2.jpg'
+import s3 from '../../../assets/services/services-3.jpg'
+import s4 from '../../../assets/services/services-4.jpg'
+import s5 from '../../../assets/services/services-5.jpg'
+import s6 from '../../../assets/services/services-6.jpg'
+
 const ServicesData = [
   {
     id: 1,
     title: "Web Design",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-1.jpg",
+    cover: s1
   },
   {
     id: 2,
     title: "Web Development",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-2.jpg",
+    cover: s2
   },
   {
     id: 3,
     title: "Photography",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-3.jpg",
+    cover: s3
   },
   {
     id: 4,
     title: "Live Support",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-4.jpg",
+    cover: s4
   },
   {
     id: 5,
     title: "Content Writing",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-5.jpg",
+    cover: s5
   },
   {
     id: 6,
     title: "Timely Deliverables",
     desc: "Lorem ipsum dolor sit amet, consecte tuer adipiscing elit enean.",
-    cover: "./assets/services/services-6.jpg",
+    cover: s6
   },
-]
+];
 
-export default ServicesData
+export default ServicesData;
