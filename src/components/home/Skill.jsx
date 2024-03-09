1e16fc5fdebac7f6f6f9c435576c65b5d71652c3
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-03-09 13:09:25 +0530

    update skill

--- src/components/home/Skill.jsx
+++ src/components/home/Skill.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
 export const Skill = () => {
-    const Progress = ({done,title}) => {
+    const Progress = ({done,title }) => {
         return (
-            <div className='Progress'>
-                <div className='progress-done' style={{ opacity: 1, width: `${done}` }}>
+            <div className='progress'>
+                <div className='progress-done' style={{ opacity: 1, width: `${done}%` }}>
                     <h4>{title}</h4>
                     <h4>{done} %</h4>
                 </div>
             </div>
         )
     }
+
+    const data = [
+        {
+            title: "Evary Day is New Challenge",
+            para: "Proficient in Java, React, HTML, CSS, JavaScript, and SQL, I specialize in both backend and frontend development. Using Java, I construct resilient backend systems, while React, HTML, CSS, and JavaScript are my go-to for crafting dynamic user interfaces.",
+
+            para1: "Additionally, my expertise in SQL allows me to effectively manage and optimize databases for optimal performance. Whether it's building robust backend infrastructures or designing intuitive user experiences, my versatile skill set ensures high-quality solutions tailored to project requirements.",
+        }
+    ]
   return (
     <>
     <section className='skill'>
@@ -22,8 +31,24 @@ export const Skill = () => {
 
         <div className='content flex'>
             <div className='left topMargin'>
-            <Progress done='70' title='HTML' />
-            </div>
+            <Progress done='95' title='HTML' />
+            <Progress done='93' title='CSS' />
+            <Progress done='70' title='JAVASCRIPT' />
+            <Progress done='40' title='REACT JS' />
+            <Progress done='50' title='JAVA' />
+        </div>
+        <div className="right mtop">
+            {data.map((val) => {
+                return (
+                    <>
+                    <h1>{val.title}</h1>
+                    <p>{val.para}</p>
+                    <p>{val.para1}</p>
+                    <button className='primary-btn'>Hire Me</button>
+                    </>
+                )
+            })}
+        </div>
         </div>
         </div>
     </section>
