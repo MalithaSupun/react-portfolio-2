ac841783cfd3f87e3d22052b0c22844484632a12
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-03-10 20:52:35 +0530

    update

--- src/components/home/work/Work.jsx
+++ src/components/home/work/Work.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Data from '../../../../src/components/home/work/Data'
+import IosShareIcon from '@mui/icons-material/IosShare';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+
+
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+    <section className='work'>
+        <div className='heading'>
+            <h3>MY WORKS</h3>
+            <h1>Some of My Competed Projects</h1>
+        </div>
+        <div className='content'>
+            {Data.map((val) => {
+                return (
+                    <>
+                    <div className='box'>
+                        <div className='img'>
+                            <img src={val.cover} alt='' />
+                        </div>
+                        <div className="overlay">
+                            <div className="text">
+                                <h2>{val.title}</h2>
+                                <p>{val.desc}</p>
+                            </div>
+                            <div className="icon">
+                                <FullscreenIcon className='iconWork'/>
+                                <IosShareIcon className='iconWork'/>
+                            </div>
+                        </div>
+                    </div>
+                    </>
+                )
+            })}
+        </div>
+    </section>
+    </>
   )
 }
 
